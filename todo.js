c1a51234b3c1b31fafc9560453938151e10f8483
nues1979 <seunmark06@gmail.com>
2020-04-06 20:48:20 +0100

    ready

--- todo.js
+++ todo.js
@@ -1,27 +1,30 @@
-
-const v = document.querySelectorAll('li')
- v.forEach((vs) => {
-   vs[i].addEventListener('click',  () =>{
-    this.classList.toggle('newli');
-} );} )
-
-
-const delet = document.querySelectorAll('li span');
-delet.forEach((delets) => {
-  delets.addEventListener('click', (delets) => {
-   this.parentElement.remove();
-  }  )
-});
-
-  let ne = document.querySelector("input")
-  ne.addEventListener('keypress',() => {
+ const inpute = document.querySelector("input")
+  inpute.addEventListener('keypress',() => {
   if(event.which===13){
-    const newTodo = ne.value.toUpperCase();
-    const div = document.createElement('li');
-    div.innerHTML = `<span class='fas fa-trash-alt'></span>${newTodo} </li>`
-  document.querySelector('ul').appendChild(div);
+      const newTodo = inpute.value.toUpperCase();
+      const newLi= document.createElement('li');
+      newLi.innerHTML = `<span class='fas fa-trash-alt'></span>${newTodo} </li>`
+    document.querySelector('ul').appendChild(newLi);
 }
   });
+
   
+  document.querySelector('input').addEventListener('focus', (event) => {
+    event.target.value = ''
+  });
+
+  document.querySelector('ul').addEventListener('click', (event) => {
+   if (event.target.tagName.toLowerCase() === 'li') {
+        event.target.classList.toggle('newli')
+  
+   };
+});
+  
+  document.querySelector('body').addEventListener('click', (event) => {
+     if (event.target.tagName.toLowerCase() === 'span') {
+          event.target.parentNode.remove();
+     
+     };
+ });
 
 
