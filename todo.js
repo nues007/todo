01b4d414b9249a2a5c1cf3fcca322415277b414c
nues1979 <seunmark06@gmail.com>
2020-04-06 13:34:22 +0100

    latest

--- todo.js
+++ todo.js
@@ -1,46 +1,27 @@
-/*$("ul").on("click", "li", function (){
-  $(this).toggleClass("newli");
-});
-$("ul").on("click","span",function(event){
-    $(this).parent().fadeOut(2000,function(){
-      $( this ).remove();
-      });
-      event.stopPropagation();
-  });
-$("input[type = 'text']").keypress(function(event){
-  if(event.which===13){
-    var newTodo =$(this).val().toUpperCase();
-    $(this).val("");
-  $("ul").append("<li><span class='fas fa-trash-alt'></span>"+ newTodo +" </li>");
-  }});
-*/
+
  let v = document.querySelectorAll('li')
- for(var i = 0; i < v.length;i++ ){
-v[i].addEventListener('click',  function(){
+ v.forEach((vs) => {
+vs[i].addEventListener('click',  () =>{
  this.classList.toggle('newli');
-} );}
+} );} )
 
 
-const fade = document.querySelectorAll('li span');
-fade.forEach(function(fades) {
-  fades.addEventListener('click', function(fades){
+const delet = document.querySelectorAll('li span');
+delet.forEach((delets) => {
+  delets.addEventListener('click', (delets) => {
    this.parentElement.remove();
   }  )
 });
 
   let ne = document.querySelector("input")
-  ne.addEventListener('keypress',function(event){
+  ne.addEventListener('keypress',() => {
   if(event.which===13){
     var newTodo =ne.value.toUpperCase();
-    //ne.value("");
-  const div = document.querySelector('li');
-
-
-
-  div.innerHTML = `
-  <li><span class='fas fa-trash-alt'></span> ${newTodo}  </li>
-  `;
-
+    const div = document.createElement('li');
+    div.innerHTML = "<span class='fas fa-trash-alt'></span>"+ newTodo +" </li>"
   document.querySelector('ul').appendChild(div);
 }
   });
+  
+
+
